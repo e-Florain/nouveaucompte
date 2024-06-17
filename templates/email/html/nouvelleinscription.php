bfc8ef84dfa65a7132ca9bda55d8a115d7091f54
Guillaume Roche <groche@guigeek.org>
2024-06-17 15:51:09 +0200

    New changes

--- templates/email/html/nouvelleinscription.php
+++ templates/email/html/nouvelleinscription.php
@@ -1,5 +1,5 @@
 Bonjour,
-une nouvelle <?php if ($todo=="create") {echo "adhésion";} if ($todo=="update") {echo "re-adhésion";}  if (isset($account_cyclos)) { ?> avec un compte numérique <?php } ?> a été enregistrée :<br>
+une nouvelle <?php if ($todo=="create") {echo "adhésion";} if ($todo=="update") {echo "re-adhésion";}  if ($account_cyclos) { ?> avec un compte numérique <?php } ?> a été enregistrée :<br>
 Nom : <?php echo $lastname; ?><br>
 Prénom : <?php echo $firstname; ?><br>
 Mail : <?php echo $email; ?><br>
@@ -8,7 +8,7 @@ Code postal : <?php echo $zip; ?><br>
 Ville : <?php echo $city; ?><br>
 Montant de l'adhésion : <?php  if (isset($nbeurosadhannuel)) { echo $nbeurosadhannuel." par an"; }
     if (isset($nbeurosadhmensuel)) { echo $nbeurosadhmensuel." par mois"; } ?><br>
-<?php if (isset($account_cyclos)) {
+<?php if ($account_cyclos) {
 echo "Change mensuel : ".$changeeuros."<br>";    
 }
 ?>
