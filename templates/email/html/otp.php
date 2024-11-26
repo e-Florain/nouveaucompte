9c885b8679921c777bc898e7ee65957cd13f57e7
Guillaume Roche <groche@guigeek.org>
2024-11-26 13:36:58 +0100

    Modify text email template for otp

--- templates/email/html/otp.php
+++ templates/email/html/otp.php
@@ -1,8 +1,17 @@
-Voici votre code pour vous connecter<br>
+<center>
+<img src="https://nouveaucompte.florain.fr/img/logo-monnaie.svg" height="64">
 <br>
-Voici votre code de vérification pour vous connecter :<br>
-<?php echo $otp; ?>
+Voici votre code de vérification pour vous connecter :
+<br>
+<b>
+<h2><?php echo $otp; ?></h2>
+</b>
+</center>
 <br>
 Nous vous avons envoyé ce code de vérification pour protéger votre compte.
 <br>
 Ce code à usage unique n'est valable que 10 minutes.
+<br>
+<i>
+Cet email vous a été envoyé par Le Florain, car vous avez créé un compte chez nous. Si vous pensez que cet email vous a été envoyé par erreur, merci de nous le signaler. 
+</i>
--- templates/email/text/otp.php
+++ templates/email/text/otp.php
@@ -1,8 +1,10 @@
-Voici votre code pour vous connecter<br>
-<br>
-Voici votre code de vérification pour vous connecter à Doctolib :<br>
+Voici votre code de vérification pour vous connecter :
+
 <?php echo $otp; ?>
+
 
 Nous vous avons envoyé ce code de vérification pour protéger votre compte.
 
 Ce code à usage unique n'est valable que 10 minutes.
+
+Cet email vous a été envoyé par Le Florain, car vous avez créé un compte chez nous. Si vous pensez que cet email vous a été envoyé par erreur, merci de nous le signaler. 
